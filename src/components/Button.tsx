4a01bfe2117609d02c72a36513776947040ebb10
kkamara <kamaracomputers@gmail.com>
2025-05-29 22:00:52 +0100

    Update Button Component design

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,20 +5,25 @@ import {
   View,
 } from 'react-native';
 import { forwardRef, } from 'react';
+import Colors from '@/constants/Colors';
 
 type ButtonProps = {
   text: string;
+  special?: boolean;
 } & React.ComponentPropsWithoutRef<typeof Pressable>;
 
 const Button = forwardRef<View | null, ButtonProps>(
-  ({ text, ...pressableProps }, ref) => {
+  ({ text, special, ...pressableProps }, ref) => {
     return (
       <Pressable
         ref={ref}
         {...pressableProps}
-        style={styles.container}
+        style={[
+          styles.container, 
+          special ? { backgroundColor: Colors.light.tint, } : { borderColor: "#4582ec", },
+        ]}
       >
-        <Text style={styles.text}>{text}</Text>
+        <Text style={[styles.text, special ? { color: "#fff", } : null]}>{text}</Text>
       </Pressable>
     );
   }
@@ -26,15 +31,16 @@ const Button = forwardRef<View | null, ButtonProps>(
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "lightblue",
+    backgroundColor: "#fff",
     padding: 15,
     alignItems: 'center',
     borderRadius: 100,
+    marginVertical: 10,
+    borderWidth: 1,
   },
   text: {
     fontSize: 16,
-    fontWeight: '600',
-    color: '#000',
+    fontWeight: 700,
   },
 });
 
